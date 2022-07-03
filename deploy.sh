ee74a3c1dcb7e9b28488d6007775362e83017f2e
Daniele Basile <asterix24@gmail.com>
2022-07-03 23:17:41 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-hugo --cleanDestinationDir --printI18nWarnings --debug --log
+#hugo --cleanDestinationDir --printI18nWarnings --debug --log
+hugo --printI18nWarnings --debug --printMemoryUsage --printPathWarnings --printUnusedTemplates -v --forceSyncStatic --buildFuture --minify
+
 
 rsync -zvrah --delete public/  caiusbonus:/home/asterix/blog/
